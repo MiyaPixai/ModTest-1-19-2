f9d9acd390aa80602f2b4d7cec5eb021fd33f7c7
Pixai <38261752+MiyaPixai@users.noreply.github.com>
2024-02-16 15:26:13 -0500

    Added ranged attack

--- src/main/java/com/pixai/testmod/entity/BokoblinBase.java
+++ src/main/java/com/pixai/testmod/entity/BokoblinBase.java
@@ -1,11 +1,10 @@
 package com.pixai.testmod.entity;
 
 
-import com.pixai.testmod.ai.BokoblinAttackGoal;
-import com.pixai.testmod.ai.DetectTargetGoal;
 import com.pixai.testmod.ai.HealWithItemGoal;
 import com.pixai.testmod.util.RandomCollection;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.sounds.SoundEvents;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.Difficulty;
 import net.minecraft.world.DifficultyInstance;
@@ -22,7 +21,11 @@ import net.minecraft.world.entity.animal.*;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.monster.Monster;
 import net.minecraft.world.entity.monster.Zombie;
+import net.minecraft.world.entity.monster.RangedAttackMob;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.projectile.AbstractArrow;
+import net.minecraft.world.entity.projectile.ProjectileUtil;
+import net.minecraft.world.item.BowItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
@@ -38,6 +41,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public abstract class BokoblinBase extends Monster implements AlertableMob, HealableMob {
+public abstract class BokoblinBase extends Monster implements AlertableMob, HealableMob, RangedAttackMob {
     private Goal bokoblinMobTarget;
     protected final RandomCollection<Item> mainHandChance;
     protected float hornChance;
@@ -48,6 +52,9 @@ public abstract class BokoblinBase extends Monster implements AlertableMob, Heal
             Items.PORKCHOP, Items.COOKED_PORKCHOP,
             Items.MUTTON, Items.COOKED_MUTTON,
             Items.CHICKEN, Items.COOKED_CHICKEN);
+
+    private final RangedBowAttackGoal<AbstractSkeleton> bowGoal = new RangedBowAttackGoal(this, 1.0, 30, 15.0F);
+    private final MeleeAttackGoal meleeGoal = new MeleeAttackGoal(this, 1.2, false);
 
     protected BokoblinBase(EntityType<? extends Monster> entityType, Level world) {
         super(entityType, world);
@@ -79,6 +86,42 @@ public abstract class BokoblinBase extends Monster implements AlertableMob, Heal
     public void alert(LivingEntity target) {
         isAlerted = true;
         this.setTarget(target);
+    public SpawnGroupData finalizeSpawn(ServerLevelAccessor levelAccessor, DifficultyInstance difficulty, MobSpawnType spawnType, @javax.annotation.Nullable SpawnGroupData groupData, @Nullable CompoundTag tag) {
+        RandomSource randomsource = levelAccessor.getRandom();
+        SpawnGroupData spawnGroupData = super.finalizeSpawn(levelAccessor, difficulty, spawnType, groupData, tag);
+//        float f = difficulty.getSpecialMultiplier();
+        this.setCanPickUpLoot(true);
+
+        this.populateDefaultEquipmentSlots(randomsource, difficulty);
+        this.populateDefaultEquipmentEnchantments(randomsource, difficulty);
+        this.reassessAttackGoal();
+
+
+        return (SpawnGroupData) spawnGroupData;
+    }
+
+    public void performRangedAttack(LivingEntity p_32141_, float p_32142_) {
+        ItemStack itemstack = this.getProjectile(this.getItemInHand(ProjectileUtil.getWeaponHoldingHand(this, (item) -> {
+            return item instanceof BowItem;
+        })));
+        AbstractArrow abstractarrow = this.getArrow(itemstack, p_32142_);
+        if (this.getMainHandItem().getItem() instanceof BowItem) {
+            abstractarrow = ((BowItem) this.getMainHandItem().getItem()).customArrow(abstractarrow);
+        }
+
+        double d0 = p_32141_.getX() - this.getX();
+        double d1 = p_32141_.getY(0.3333333333333333) - abstractarrow.getY();
+        double d2 = p_32141_.getZ() - this.getZ();
+        double d3 = Math.sqrt(d0 * d0 + d2 * d2);
+        abstractarrow.shoot(d0, d1 + d3 * 0.20000000298023224, d2, 1.6F, (float) (14 - this.level.getDifficulty().getId() * 4));
+        this.playSound(SoundEvents.ARROW_SHOOT, 1.0F, 1.0F / (this.getRandom().nextFloat() * 0.4F + 0.8F));
+        this.level.addFreshEntity(abstractarrow);
+    }
+
+    protected AbstractArrow getArrow(ItemStack p_32156_, float p_32157_) {
+        return ProjectileUtil.getMobArrow(this, p_32156_, p_32157_);
+    }
+
     }
 
     @Override
@@ -106,12 +149,9 @@ public abstract class BokoblinBase extends Monster implements AlertableMob, Heal
         });
 
         this.goalSelector.addGoal(1, new FloatGoal(this));//float
-        this.goalSelector.addGoal(2, new HealWithItemGoal(this));//if low hp, flee and heal
-        //this.goalSelector.addGoal(2,new BokoblinAttackGoal(this,1.5,false)); //Detect and alart if player in range
-        this.goalSelector.addGoal(3, new BokoblinAttackGoal(this, 1.5, false));
-        this.goalSelector.addGoal(3, new DetectTargetGoal(this, true));//if alerted, hunt target down
         //this.goalSelector.addGoal(5, new ScavengeGoal(this, true));//Scavenge goal, seek healing items from the floor
         //this.goalSelector.addGoal(6, new DropInContainerGoal(this, true));//if inventory filled and container exists nearby
+        this.goalSelector.addGoal(3, new HealWithItemGoal(this));//if low hp, flee and heal
 
         this.goalSelector.addGoal(8, new RandomLookAroundGoal(this));//random idling
         this.goalSelector.addGoal(10, new WaterAvoidingRandomStrollGoal(this, 0.5D));
@@ -121,34 +161,23 @@ public abstract class BokoblinBase extends Monster implements AlertableMob, Heal
         this.targetSelector.addGoal(6, this.bokoblinMobTarget);//hunt passive mobs
     }
 
-    @javax.annotation.Nullable
-    public SpawnGroupData finalizeSpawn(ServerLevelAccessor levelAccessor, DifficultyInstance difficulty, MobSpawnType spawnType, @javax.annotation.Nullable SpawnGroupData groupData, @Nullable CompoundTag tag) {
-        RandomSource randomsource = levelAccessor.getRandom();
-        SpawnGroupData spawnGroupData = super.finalizeSpawn(levelAccessor, difficulty, spawnType, groupData, tag);
-//        float f = difficulty.getSpecialMultiplier();
-        this.setCanPickUpLoot(true);
+    private void reassessAttackGoal() {
+        if (this.level != null && !this.level.isClientSide) {
+            ItemStack itemstack = this.getItemInHand(ProjectileUtil.getWeaponHoldingHand(this, (item) -> {
+                return item instanceof BowItem;
+            }));
+            if (itemstack.is(Items.BOW)) {
+                int i = 20;
+                if (this.level.getDifficulty() != Difficulty.HARD) {
+                    i = 40;
+                }
 
-        this.populateDefaultEquipmentSlots(randomsource, difficulty);
-        this.populateDefaultEquipmentEnchantments(randomsource, difficulty);
-
-
-        return (SpawnGroupData) spawnGroupData;
+                this.bowGoal.setMinAttackInterval(i);
+                this.goalSelector.addGoal(4, this.bowGoal);
+            } else {
+                this.goalSelector.addGoal(4, this.meleeGoal);
+            }
+        }
     }
 
-    @Override
-    public boolean wantsToPickUp(ItemStack item) {
-
-        return super.wantsToPickUp(item);
-    }
-
-    @Override
-    protected void pickUpItem(ItemEntity p_21471_) {
-        super.pickUpItem(p_21471_);
-
-    }
-
-    @Override
-    public boolean doHurtTarget(Entity target) {
-        return super.doHurtTarget(target);
-    }
 }
